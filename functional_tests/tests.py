e677808e43d327a32be1fd481032c2d558c7e2b2
王玥 <moonwang1999@126.com>
2020-05-24 15:29:47 +0800

    first steps of FT layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -11,6 +11,8 @@ class NewVisitorTest(LiveServerTestCase):
           self.browser = webdriver.Firefox()
       def tearDown(self):
           self.browser.quit()
+
+
       def wait_for_row_in_list_table(self, row_text):
           start_time = time.time()
           while True:
@@ -103,6 +105,27 @@ class NewVisitorTest(LiveServerTestCase):
           # Satisfied , she goes back to sleep
 
           # self.browser.quit() 111
+      def test_layout_and_styling(self):
+          self.browser.get(self.live_server_url)
+          self.browser.set_window_size(1024, 768)
+
+          inputbox = self.browser.find_element_by_id('id_new_item')
+          self.assertAlmostEqual(
+                  inputbox.location['x'] + inputbox.size['width'] / 2,
+                  512,
+                  delta=10
+           )
+          inputbox.send_keys('testing')
+          inputbox.send_keys(Keys.ENTER)
+          self.wait_for_row_in_list_table('1: testing')
+          inputbox = self.browser.find_element_by_id('id_new_item')
+          self.assertAlmostEqual(
+                  inputbox.location['x'] + inputbox.size['width'] / 2,
+                  512,
+                  delta=10
+           )
+
+
 
 
 if __name__== '__main__':
